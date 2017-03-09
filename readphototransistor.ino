e58afd1224b0e160962d4cc6fe5d88401b4f0da9
pipizzo <pipizzo@ulb.ac.be>
2017-03-09 13:01:25 +0100

    Add files via upload

---
+++ readphototransistor.ino
@@ -0,0 +1,17 @@
+int photoTran = A2;
+int reading = 0;
+int led = 2;
+
+
+void setup(){
+  pinMode(photoTran,INPUT);
+  pinMode(led,OUTPUT);
+  digitalWrite(led,HIGH);
+  Serial.begin(9600);
+}
+
+void loop(){
+  reading = analogRead(photoTran);
+  Serial.println(reading);
+  delay(100);
+}
